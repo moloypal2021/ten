5fb8de28961ad3f203006a72d41d12afdc4a0e74
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2022-03-31 19:24:25 +0600

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        cyan: {
+          10: "#425372",
+        },
+        gray: {
+          10: "#BBAFAF",
+        },
+        red: {
+          10: "#E81919",
+        },
+        green: {
+          10: "#61CE70",
+        },
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
