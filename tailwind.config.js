9440246da3166263af4d7f4274cadf4dd973f238
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2022-04-01 13:42:56 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,18 @@ module.exports = {
         },
         gray: {
           10: "#BBAFAF",
+          20: "#3F4551",
+          30: "#545454",
         },
         red: {
           10: "#E81919",
         },
         green: {
           10: "#61CE70",
+        },
+        blue: {
+          10: "#4D7ACD",
+          20: "#5083DF",
         },
       },
       fontFamily: {
